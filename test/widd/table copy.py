2cbc165d4d9151bdeffaee00029eade9c686a821
hongsusoo <hyhgoodgo7@gmail.com>
2021-06-09 02:43:43 +0900

    Applied WidgetTable by Using Pandas, add,del,save button ok, test copy.py

--- test/widd/table copy.py
+++ test/widd/table copy.py
@@ -8,6 +8,7 @@ class MyApp(QWidget):
         self.read_exl()
         self.initUI()
         self.set_exl()
+        self.r=0
 
     def initUI(self):
         
@@ -21,12 +22,9 @@ class MyApp(QWidget):
         self.delRow = QPushButton('행삭제')
         self.tableWidget.cellClicked.connect(self.set_label)
         self.delRow.clicked.connect(self.del_row)
-        self.clear = QPushButton('Clear')
-        self.clear.clicked.connect(self.tableWidget.clear)
-
+        self.clear = QPushButton('저장')
+        self.clear.clicked.connect(self.save_data)
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-
-        
 
         hbox = QHBoxLayout()
         hbox.addWidget(self.addRow)
@@ -54,26 +52,38 @@ class MyApp(QWidget):
         for i in range(self.row):
             for j in range(self.col):
                 if type(self.df.loc[i][j])!=str: 
-                    temp=str(int(self.df.loc[i][j]))+"원"
+                    temp=str(int(self.df.loc[i][j]))
                 else : temp = self.df.loc[i][j]
                 self.tableWidget.setItem(i,j,QTableWidgetItem(temp))
 
     def add_row(self): #맨 아래 행 추가
         self.row += 1
         self.tableWidget.setRowCount(self.row)
+        df2 = pd.DataFrame([['' for _ in range(self.df.shape[1])]],columns=self.df.columns)
+        self.df = self.df.append(df2,ignore_index=True)     
 
-    def del_row(self, r): #선택 행 삭제
-        print(self.row)
-        try:
-            self.df = self.df.drop(3,axis=0)
-        except Exception as e: print(e)
-        except Exception as e: print(e)
+    def del_row(self): #선택 행 삭제
+        self.tableWidget.removeRow(self.r)
+        self.df.iloc[self.r] = None
+        self.df = self.df.dropna()
+        self.df.reset_index(inplace=True)
+        self.df.drop(["index"],axis=1,inplace=True)
         self.col = self.df.shape[1]
         self.row = self.df.shape[0]
-        print(self.df)
-        print(self.row)
-        self.set_exl()
         
+    def save_data(self):
+        
+        print(self.row)
+        print(self.col)
+        # temp.columns = self.df.columns
+        try:        
+            for i in range(self.row):
+                for j in range(self.col):
+                    self.df.loc[i][j] = self.tableWidget.item(i,j).text()
+        except Exception as e : print(e)
+        self.df.to_excel("C:/코린이/04. 개인 프로젝트/01.angle_price/data/data_angle.xlsx",sheet_name = "angle")
+        
+
     def set_label(self, row, column): 
         item = self.tableWidget.item(row, column)
         try:
